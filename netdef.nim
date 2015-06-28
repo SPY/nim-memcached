b150af995744821af1b7829a7bfa12619da7af6a
Ilya Rezvov <rezvov.ilya@undev.ru>
2015-06-28 12:45:57 +0300

    more macro refactoring

--- netdef.nim
+++ netdef.nim
@@ -7,7 +7,11 @@ export sockets.ntohs
 export sockets.htonl
 export sockets.htons
 
-const bigInts = ["int16", "uint16", "int32", "uint32", "int", "uint", "int64", "uint64"]
+type FieldType = tuple[isEnum: bool, storeType, pubType: string, default: NimNode]
+type Field = tuple[pub: bool, name: string, fieldType: FieldType]
+type NetworkStruct = tuple[pub: bool, name: string, fields: seq[Field]]
+
+const bigInts = ["int16", "uint16", "int32", "uint32", "int64", "uint64"]
 
 proc isBigInt(strType: string): bool =
   return strType in bigInts
@@ -26,11 +30,6 @@ proc getterTypes(strType: string): tuple[t, procName: string] =
   of "int16", "uint16": return ("int16", "ntohs")
   of "int32", "uint32": return ("int32", "ntohl")
   of "int64", "uint64": return ("int64", "ntohll")
-  of "int", "uint":
-    case sizeof(int):
-    of 4: return ("int32", "ntohl")
-    of 8: return ("int64", "ntohll")
-    else: discard
 
 proc setterTypes(strType: string): tuple[t, procName: string] =
   case strType:
@@ -38,18 +37,36 @@ proc setterTypes(strType: string): tuple[t, procName: string] =
   of "int16", "uint16": return ("int16", "htons")
   of "int32", "uint32": return ("int32", "htonl")
   of "int64", "uint64": return ("int64", "htonll")
-  of "int", "uint":
-    case sizeof(int):
-    of 4: return ("int32", "htonl")
-    of 8: return ("int64", "htonll")
-    else: discard
 
-proc makeGetter(typeName, fieldName, fieldType, enumType: NimNode, isEnum, isPub: bool): tuple[get, getref: NimNode] {. compileTime .} =
-  let (convType, conv) = getterTypes($fieldType)
-  let (convTypeIdent, convIdent) = (ident(convType), ident(conv))
-  let retType = if isEnum: enumType else: fieldType
-  let capFieldName = ident("big" & capitalize($fieldName))
-  let fieldNameWithPub = if isPub: postfix(fieldName, "*") else: fieldName
+proc `&`(str: string): NimNode {. compileTime .} =
+  result = ident(str)
+
+proc makeCounstructor(def: NetworkStruct): NimNode {. compileTime .} =
+  let ctorIdent = ident("new" & capitalize(def.name))
+  let ctorName = if def.pub: postfix(ctorIdent, "*") else: ctorIdent
+  let value = genSym(nskVar, "value")
+  let typeName = &def.name
+  var ctor = quote do:
+    proc `ctorName`(): ref `typeName` =
+      var `value` = new(`typeName`)
+  var ctorParams = ctor[0][3]
+  var ctorBody = ctor[0][6]
+  for field in def.fields:
+    let fieldName = &field.name
+    let assign = quote do:
+      `value`.`fieldName` = `fieldName`
+    ctorParams.add(newIdentDefs(fieldName, &field.fieldType.pubType, field.fieldType.default))
+    ctorBody.add(assign[0])
+  ctorBody.add(value)
+  ctor[0]
+
+proc makeGetter(name: string, field: Field): tuple[get, getref: NimNode] {. compileTime .} =
+  let (convType, conv) = getterTypes(field.fieldType.storeType)
+  let (convTypeIdent, convIdent) = (&convType, &conv)
+  let retType = &field.fieldType.pubType
+  let capFieldName = ident("big" & capitalize(field.name))
+  let fieldNameWithPub = if field.pub: postfix(&field.name, "*") else: &field.name
+  let typeName = &name
   let getterProc = quote do:
     proc `fieldNameWithPub`(self: `typeName`): `retType` =
       self.`capFieldName`.`convTypeIdent`.`convIdent`.`retType`
@@ -58,16 +75,17 @@ proc makeGetter(typeName, fieldName, fieldType, enumType: NimNode, isEnum, isPub
       self[].`capFieldName`.`convTypeIdent`.`convIdent`.`retType`
   (getterProc[0], getterProcRef[0])
 
-proc makeSetter(typeName, fieldName, fieldType, enumType: NimNode, isEnum, isPub: bool): NimNode {. compileTime .} =
-  let valType = if isEnum: enumType else: fieldType
-  let (convType, conv) = setterTypes($fieldType)
-  let (convTypeIdent, convIdent) = (ident(convType), ident(conv))
-  let retType = if isEnum: enumType else: fieldType
-  let capFieldName = ident("big" & capitalize($fieldName))
+proc makeSetter(name: string, field: Field): NimNode {. compileTime .} =
+  let valType = &field.fieldType.pubType
+  let (convType, conv) = setterTypes(field.fieldType.storeType)
+  let (convTypeIdent, convIdent) = (&convType, &conv)
+  let capFieldName = ident("big" & capitalize(field.name))
   var setterName = newNimNode(nnkAccQuoted)
-  setterName.add(fieldName)
-  setterName.add(ident("="))
-  let setterNameWithPub = if isPub: postfix(setterName, "*") else: setterName
+  setterName.add(&field.name)
+  setterName.add(&"=")
+  let setterNameWithPub = if field.pub: postfix(setterName, "*") else: setterName
+  let typeName = &name
+  let fieldType = &field.fieldType.storeType
   let setterProc = quote do:
     proc `setterNameWithPub`(self: ref `typeName`, value: `valType`) =
       self.`capFieldName` = ord(value).`convTypeIdent`.`convIdent`.`fieldType`
@@ -97,10 +115,6 @@ proc parseHeader(header: NimNode): tuple[pub: bool, name: string] {. compileTime
     result = (access == "pub", $def[1])
   else:
     assert(false, "Wrong network struct header format")
-
-type FieldType = tuple[isEnum: bool, storeType, pubType: string, default: NimNode]
-type Field = tuple[pub: bool, name: string, fieldType: FieldType]
-type NetworkStruct = tuple[pub: bool, name: string, fields: seq[Field]]
 
 proc parseFieldType(fieldType: NimNode): FieldType {. compileTime .} =
   case fieldType[0].kind
@@ -151,28 +165,6 @@ proc parseDefAst(header: NimNode, body: NimNode): NetworkStruct {. compileTime .
     fields.add(parseField(field))
   result = (access, name, fields)
 
-proc `&`(str: string): NimNode {. compileTime .} =
-  result = ident(str)
-
-proc makeCounstructor(typeName: NimNode, fields: seq[Field], isPub: bool): NimNode {. compileTime .} =
-  let ctorIdent = ident("new" & capitalize($typeName))
-  let ctorName = if isPub: postfix(ctorIdent, "*") else: ctorIdent
-  let value = genSym(nskVar, "value")
-  var ctor = quote do:
-    proc `ctorName`(): ref `typeName` =
-      var `value` = new(`typeName`)
-  var ctorParams = ctor[0][3]
-  var ctorBody = ctor[0][6]
-  for field in fields:
-    let fieldName = &field.name
-    let assign = quote do:
-      `value`.`fieldName` = `fieldName`
-    let valType = &field.fieldType.pubType
-    ctorParams.add(newIdentDefs(fieldName, valType, field.fieldType.default))
-    ctorBody.add(assign[0])
-  ctorBody.add(value)
-  ctor[0]
-
 macro network*(command, body: stmt): stmt {. immediate .} =
   let def = parseDefAst(command, body)
   let typeName = if def.pub: postfix(&def.name, "*") else: &def.name
@@ -183,29 +175,15 @@ macro network*(command, body: stmt): stmt {. immediate .} =
     if isBigInt(field.fieldType.storeType) or field.fieldType.isEnum:
       let capFieldName = ident("big" & capitalize(field.name))
       recList.add(newIdentDefs(capFieldName, &field.fieldType.storeType))
-      let (get, getref) = makeGetter(
-        &def.name,
-        &field.name,
-        &field.fieldType.storeType,
-        &field.fieldType.pubType,
-        field.fieldType.isEnum,
-        field.pub
-      )
+      let (get, getref) = makeGetter(def.name, field)
       result.add(get)
       result.add(getref)
-      result.add(makeSetter(
-        &def.name,
-        &field.name,
-        &field.fieldType.storeType,
-        &field.fieldType.pubType,
-        field.fieldType.isEnum,
-        field.pub
-      ))
+      result.add(makeSetter(def.name, field))
     else:
       let fieldName = if field.pub: postfix(&field.name, "*") else: &field.name
       recList.add(newIdentDefs(fieldName, &field.fieldType.storeType))
   result[0][0][2][2] = recList
-  result.add(makeCounstructor(&def.name, def.fields, def.pub))
+  result.add(makeCounstructor(def))
 
 when isMainModule:
   type Opcode = enum One = 3, Two = 7, Three = 8
